e1ed8f4705d5adb4e476e264b4971fff402584e4
Felipe Cavalca <contato@felipecavalca.dev>
2025-07-20 18:23:24 -0300

    Excluindo atributos do index

--- api/Controller/payments.php
+++ api/Controller/payments.php
@@ -2,32 +2,17 @@
 
 namespace Bifrost\Controller;
 
-use Bifrost\Attributes\Details;
-use Bifrost\Attributes\Method;
-use Bifrost\Attributes\RequiredFields;
-use Bifrost\Class\HttpResponse;
-use Bifrost\Class\Payment;
-use Bifrost\Core\Database;
 use Bifrost\Core\Post;
 use Bifrost\Core\Queue;
 use Bifrost\DataTypes\Money;
 use Bifrost\DataTypes\UUID;
-use Bifrost\Enum\Field;
 use Bifrost\Interface\ControllerInterface;
 use Bifrost\Tasks\savePayment;
 
 class Payments implements ControllerInterface
 {
 
-    #[Method("POST")]
-    #[RequiredFields([
-        "correlationId" => Field::UUID,
-        "amount" => Field::MONEY,
-    ])]
-    #[Details([
-        "description" => "Create a new payment"
-    ])]
-    public function index(): HttpResponse
+    public function index()
     {
         $queue = new Queue();
         $post = new Post();
@@ -35,13 +20,5 @@ class Payments implements ControllerInterface
         $amount = new Money(value: $post->amount);
         $savePaymentTask = new savePayment(id: $id, amount: $amount);
         $queue->addToEnd(task: $savePaymentTask);
-
-        return HttpResponse::created(
-            objName: "payment",
-            data: [
-                "id" => (string) $id,
-                "amount" => $amount->getValue(),
-            ]
-        );
     }
 }
